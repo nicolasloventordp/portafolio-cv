d38d5acc6134785c85b8d416027ef3c3131fc770
Nicolas Lo Vento <nicolaslovento.94@hotmail.com>
2022-06-07 09:03:03 -0300

    header init

---
+++ src/views/Header/components/HeaderBox/index.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export default function HeaderBox() {
+    return (
+        <div className="header-box">
+            <div className="header-box-logo"></div>
+            <div className="header-box-sections"></div>
+        </div>
+    );
+}
---
+++ src/views/Header/index.js
@@ -0,0 +1,10 @@
+import HeaderBox from "./components/HeaderBox";
+import "./style.css";
+
+export default function Header() {
+    return (
+        <div className="header">
+            <HeaderBox />
+        </div>
+    );
+}
